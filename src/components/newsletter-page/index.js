554c6d395eb413ab81e7aef5913a3a8c9389db10
DaniBlaney <daniblaney@gmail.com>
2019-08-17 01:23:33 -0400

    added two articles to newsletter page

--- src/components/newsletter-page/index.js
+++ src/components/newsletter-page/index.js
@@ -11,9 +11,108 @@ const Newsletter = () => {
 
       <Container fluid>
         <Row className="justify-content-md-center">
+          <div className="wordWrapper">
+            <Col>
+              <h3>August Newsletter</h3><hr />
+              <h4>Backpack Safety is Back-to-School Issue</h4>
+              <h5>Chiropractic Research</h5>
+              <Card.Img className="wordWrap" style={{ width: 145 }} src="./images/backpack.png" />
+              <p>Concern over children and their backpacks continues to grow.
+                 An article appearing in the The Times Herald features this
+                 problem by noting "Trudging their way around the school campus
+                 or to the bus stop, hunched-over kids could be dealing themselves
+               a lifetime of back pain, experts warn."</p>
+              <p>The US Consumer Product Safety Commission estimates that 6,512
+                emergency room visits each year result from injuries related to
+                book bags. CPSC also cites the statistic that backpack-related
+                injuries are up 256 percent since 1996. The issue has become so
+                widespread, that the California State Assembly passed legislation
+                that forces school districts to develop ways of reducing the weight
+              of students' backpacks. Other states are also considering similar legislation.</p>
+              <p>In an online survey conducted last year of more than 200 chiropractors
+              responding from across North America at www.backpacksafe.com, it was learned that:</p>
+              <p>89 percent of chiropractors surveyed responded that they have seen patients (ages 5-18)
+                reporting back, neck or shoulder pain caused by heavy backpacks.
+                71 percent of chiropractors presently seeing such patients responded that they are
+                currently seeing one to four patients (ages 5-18) reporting back, neck or shoulder
+              pain caused by heavy backpacks.</p>
+              <p>20 percent of chiropractors presently seeing such patients responded that they
+                are currently seeing five to nine patients (ages 5-18) reporting back, neck or
+              shoulder pain caused by heavy backpacks.</p>
+              <p>9 percent of chiropractors presently seeing such patients responded that they
+                are currently seeing 10 or more patients (ages 5-18) due to back, neck or shoulder
+              caused by heavy backpacks.</p>
+              <p>The American Chiropractic Association has offered the following tips to help
+              prevent backpack problems in school children. Those tips include:</p>
+              <p>Make sure your child's backpack weighs no more than 5 to 10 percent of his or her body weight.</p>
+              <p>The backpack should never hang more than four inches below the waistline.
+                Urge your child to wear both shoulder straps, and wide, padded straps are very important.
+                The shoulder straps should be adjustable so the backpack can be fitted to your child's body.
+                The over-packing of backpacks was featured in a recent study conducted in Italy. In this study
+                it was found that the average child carries a backpack that would be the equivalent of a 39-pound
+              burden for a 176-pound man, or a 29-pound load for a 132-pound woman.</p>
+            </Col>
+          </div>
+        </Row>
+        <Row>
+        <div className="wordWrapper">
           <Col>
-
+          <h4>Chiropractic Treatment for Asthma</h4>
+              <h5>Dr. Ryan Marshall, D.C.</h5>
+              <Card.Img className="wordWrap" style={{ width: 175 }} src="./images/womanasthma.png" />
+              <p>What is Asthma?</p>
+              <p>Asthma is a breathing condition that affects many people of all ages. Some
+                suffer with asthma for their entire life from childhood, while others can develop
+                asthma later in life (adult onset asthma). Unfortunately, asthma does not have a
+                cure, and depending on the severity of the condition, it can play a debilitating
+                role in a person's life.</p>
+              <p>When people breathe, air is inhaled and exhaled into and out of the lungs, so
+                that oxygen can be taken into the body and carbon dioxide can be expelled.
+                Within the lungs, the air travels through smaller and smaller tubes until it
+                reaches the bronchioles. The bronchioles have a lining that is only one cell thick,
+                which allows the gas exchange between the blood and the air within the lungs.</p>
+              <p>During an asthma attack, the bronchioles can spasm, swell, fill with liquid or any
+                combination of these effects. As such, the air does not reach the blood as well,
+                which means the person feels like he or she cannot breath. At the mildest, an
+                asthma attack can cause an unpleasant feeling of not being able to catch your
+                breath. At the worst, these attacks can prove to be fatal. As such, finding a
+                successful method of treatment for this condition is extremely important.</p>
+              <p>Treating Asthma</p>
+              <p>In many cases, when a person suffers from an asthma attack, some type of
+                medication is used to reduce the symptoms of the attack. There are basically
+                two types of medication prescribed for those who suffer from asthma. Inhalers
+                are often used to provide temporary relief during an attack, and other medicines,
+                like steroids, are used by some to prevent asthma attacks from happening. As more
+                and more research is done into asthma and what causes the condition, further
+                treatment options besides medication are becoming available.</p>
+              <p>One non-medicinal method of treating asthma is through chiropractic treatments.
+                The goal of any chiropractic treatment is to find the root cause of the symptoms
+                and then treat that cause and not the symptoms themselves. In the case of asthma,
+                the asthma attacks are actually the symptoms and the chiropractor works to
+                determine what is causing the asthma so that it can be corrected. By correcting
+                this root cause, the chiropractor will be treating the patient's asthma as well.</p>
+              <p>There are many causes for asthma, so it is important that the right cause is found.
+                In those that have adult onset asthma, it is not uncommon to find that the asthma
+                started after some kind of injury. The injury could be anything from a car accident
+                to a fall or an injury while playing a sport. By pinpointing this injury or trauma
+                and making any corrections through chiropractic treatments, the asthma in the patient
+                can be reduced or even e</p>
+              <p>Even in children or those who have had asthma since they were children, there have
+                been studies that show that chiropractic treatments can help reduce the severity of the
+                asthma. Based on the symptoms of asthma, there is a firm belief that the nervous system
+                has a role in asthma attacks. As such, chiropractic treatments look for ways to reduce
+                any pressure on the nervous system that may be affecting the lungs. One such example is
+                a misalignment of the spine. If the spine is misaligned, this in turn can affect the
+                nerves of the lungs. If these nerves are not functioning properly, then it is likely
+                that the lungs won't function properly either. By realigning the spine, the pressure on
+                the nerves will be reduced, meaning the lungs can function better.</p>
+              <p>Chiropractic treatments have been used both to prevent asthma and during asthma attacks.
+                Some people have found that if they have a chiropractic treatment while suffering an asthma
+                attack, the treatment stops the attack and they are able to breathe normally without having
+                to use any medication. There are many people who suffer from asthma and have found that with
+                regular chiropractic treatments, their symptoms are either reduced or completely eliminated.</p>
           </Col>
+          </div>
         </Row>
       </Container>
       <FooterComponent />
