384d6ed70b6c4fc0d968353b0445ef74f96427db
DaniBlaney <daniblaney@gmail.com>
2019-08-17 09:15:01 -0400

    fixed navbar collapse

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Nav, NavDropdown, NavItem, Navbar, NavbarBrand, NavLink, Container, Button } from 'react-bootstrap';
+import { Nav, NavDropdown, NavItem, Navbar, NavbarBrand, NavLink, NavbarToggle, NavbarCollapse, Container, Button } from 'react-bootstrap';
 
 const NavBarComponent = () => {
   return (<>
     <Container fluid>
-      <Navbar className="navbar" id="custom-nav" sticky="top" bg="light" variant="light">
+      <Navbar className="navbar" id="custom-nav" sticky="top" bg="light" variant="light" expand="lg">
         <Navbar.Brand href="/">
           <img
             alt=""
@@ -15,10 +15,13 @@ const NavBarComponent = () => {
           />
           {' Samsara Chiropractic Wellness Center'}
         </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="justify-content-right">
           <Nav.Link href="/about">About Us</Nav.Link>
           <Nav.Link href="/clinicinfo">Clinic Info</Nav.Link>
         </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </Container>
   </>
