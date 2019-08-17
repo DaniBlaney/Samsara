92b75d0d3fe99dd359870c18c66c8d4c16656e44
DaniBlaney <daniblaney@gmail.com>
2019-08-16 22:20:15 -0400

    added text to home

--- src/components/home-page/index.js
+++ src/components/home-page/index.js
@@ -14,6 +14,13 @@ const HomePage = () => {
         <Row className="justify-content-md-center">
           <Col md={4}>
             <Card.Img className="imgStyle" src="./images/redmond.png" />
+          </Col>
+          <Col>
+            <h2>Jennifer E. Redmond, D.C.</h2>
+            <p>Dr. Redmond's office is a family practice serving mainly mothers and children. We pride ourselves on our warm atmosphere and variety of techniques,
+              allowing us to cater to your specific needs and attain the best results possible.</p>
+            <p>Samsara is the Buddhist term for life, death, and rebirth. Like the caterpillar that must let go of its previous way of life before emerging as
+              the butterfly, we, too, can transform into a more beautiful version of ourselves if we let go...</p>
             </Col>
             <Col md={8}>
           </Col>
