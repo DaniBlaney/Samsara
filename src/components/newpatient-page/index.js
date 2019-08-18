c7e188024fade41bf449488d13d8d88ed91dbfe5
DaniBlaney <daniblaney@gmail.com>
2019-08-18 15:42:21 -0400

    new patient forms

--- src/components/newpatient-page/index.js
+++ src/components/newpatient-page/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Row, Col, Card, Body, Title, Text, Nav, Navbar, Dropdown, ButtonGroup, NavItem, Link, Brand, Button, FormControl, Form, Image, Container } from 'react-bootstrap';
+import { Row, Col, Card, Body, Title, Text, Jumbotron, Nav, Navbar, Dropdown, ButtonGroup, NavItem, Link, Brand, Button, FormControl, Form, Image, Container } from 'react-bootstrap';
 import NavBarComponent from "../navbar";
 import FooterComponent from '../footer';
 
@@ -10,7 +10,26 @@ const NewPatient = () => {
       <NavBarComponent />
       <div className="gradient">
         <Container fluid>
-          <h3 className="newPatient">New Patient Forms</h3>
+          <Jumbotron className="jumboTron">
+            <Container>
+              <h3 className="newPatient">New Patient Forms</h3>
+              <Row className="justify-content-md-center">
+                <Col>
+                  <p>
+                    Please print this form and fill it out at home. Bring it in on your first visit. See you in the clinic!</p>
+                  <a href="http://www.doctorredmond.com/images/common/patient_signup.pdf" target="_blank">Download PDF Sign-up form</a>
+                </Col>
+              </Row>
+              <hr />
+              <Row>
+                <Col>
+                  <p>Click below to fill out our wellness check questionnaire</p>
+                  <a href="https://www.wellnesscheckonline.com/check1.php" target="_blank">Wellness Check Questionaire</a>
+                </Col>
+              </Row>
+            </Container>
+          </Jumbotron>
+          {/* <h3 className="newPatient">New Patient Forms</h3>
           <Row className="justify-content-md-center">
             <Col>
               <p>
@@ -18,13 +37,13 @@ const NewPatient = () => {
               <a href="http://www.doctorredmond.com/images/common/patient_signup.pdf" target="_blank">Download PDF Sign-up form</a>
             </Col>
           </Row>
-          <hr/>
+          <hr />
           <Row>
             <Col>
               <p>Click below to fill out our wellness check questionnaire</p>
               <a href="https://www.wellnesscheckonline.com/check1.php" target="_blank">Wellness Check Questionaire</a>
             </Col>
-          </Row>
+          </Row> */}
         </Container>
       </div>
       <div className="fixed-bottom">
